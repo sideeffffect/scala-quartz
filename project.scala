0b9b35882d7daf6fdd3d304f015fc422a1dff12d
Ondra Pelech <ondra.pelech@gmail.com>
2025-05-27 00:24:52 +0200

    Init

---
+++ project.scala
@@ -0,0 +1,9 @@
+//> using scala 2.13.16 3.3.6
+//> using dep org.tpolecat::doobie-hikari:1.0.0-RC5
+//> using publish.organization com.github.sideeffffect
+//> using publish.name scala-quartz
+//> using publish.version 0.1.0-SNAPSHOT
+//> using publish.url https://github.com/sideeffffect/scala-quartz/
+//> using publish.license Apache-2.0
+//> using publish.developer "Ondra Pelech| |https://github.com/sideeffffect/scala-quartz/"
+//> using publish.repository central
