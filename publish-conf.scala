d5562ba4e12fcf4e7b585532ce6ec4610d447ddb
Ondra Pelech <ondra.pelech@gmail.com>
2025-05-27 12:54:05 +0200

    publish

---
+++ publish-conf.scala
@@ -0,0 +1,7 @@
+//> using publish.ci.computeVersion git:tag
+//> using publish.ci.repository central-s01
+//> using publish.ci.user env:PUBLISH_USER
+//> using publish.ci.password env:PUBLISH_PASSWORD
+//> using publish.ci.secretKey env:PUBLISH_SECRET_KEY
+//> using publish.ci.publicKey env:PUBLISH_PUBLIC_KEY
+//> using publish.versionControl github:sideeffffect/scala-quartz
