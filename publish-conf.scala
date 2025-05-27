86dc43c4e596b642c8dad096d9842256677b40d0
Ondra Pelech <ondra.pelech@gmail.com>
2025-05-27 13:02:03 +0200

    PUBLISH_SECRET_KEY_PASSWORD

--- publish-conf.scala
+++ publish-conf.scala
@@ -3,5 +3,6 @@
 //> using publish.ci.user env:PUBLISH_USER
 //> using publish.ci.password env:PUBLISH_PASSWORD
 //> using publish.ci.secretKey env:PUBLISH_SECRET_KEY
+//> using publish.ci.secretKey env:PUBLISH_SECRET_KEY_PASSWORD
 //> using publish.ci.publicKey env:PUBLISH_PUBLIC_KEY
 //> using publish.versionControl github:sideeffffect/scala-quartz
